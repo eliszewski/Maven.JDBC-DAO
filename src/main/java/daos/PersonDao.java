21554b3a36e58de656ed25883037b368aae2d4a3
Eryk <eryk@Zipcoders-MacBook-Pro-71.local>
2022-12-08 09:30:41 -0500

    add findby ID

--- src/main/java/daos/PersonDao.java
+++ src/main/java/daos/PersonDao.java
@@ -3,14 +3,38 @@ package daos;
 import models.Person;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 public class PersonDao implements PersonDaoInterface{
     Connection connection = ConnectionFactory.getConnection();
 
+    public Person extractCarsFromResultSet(ResultSet rs) throws SQLException {
+        Person person = new Person();
+
+        person.setId(rs.getInt("ID"));
+        person.setLastName(rs.getString("LastName"));
+        person.setFirstName(rs.getString("FirstName"));
+        person.setAge(rs.getInt("Age"));
+        person.setJob(rs.getString("Job"));
+        return person;
+    }
+
     @Override
     public Person findById(int id) {
-        return null;
+        try {
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM Persons WHERE id = " + id + ";");
+
+            if (rs.next()){
+                return extractCarsFromResultSet(rs);
+            }
+            return null;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
